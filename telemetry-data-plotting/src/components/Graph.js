825f5f31c73219939f145dc88603477aa198104a
Miltiadis Stouras <m.stouras@gmail.com>
2018-03-27 00:27:28 +0300

    Added axis titles in Graph.js

--- telemetry-data-plotting/src/components/Graph.js
+++ telemetry-data-plotting/src/components/Graph.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-const SinglePlot = ({dataToPlot,packets,title}) => (
+const SinglePlot = ({dataToPlot, packets, title, units}) => (
   <Plot
-  data={
-    [{
-      x: packets,
-      y: dataToPlot,
-      type: 'scatter',
-      mode: 'lines+points',
-      marker: {color: 'red'},
+    data={
+      [{
+        x: packets,
+        y: dataToPlot,
+        type: 'scatter',
+        mode: 'lines+points',
+        marker: {color: 'red'},
     }]}
-  layout={{width: 420, height: 400, title: title }}
-/> );
+    layout={
+      {width: 420,
+      height: 400,
+      title: title,
+      yaxis: { title: `(${units})`},
+      xaxis: { title: "time(s)"},
+    }}
+  /> );
 
 
 export { SinglePlot };
