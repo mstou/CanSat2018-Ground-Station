40cafbf9784f498718dc6068842dd3984e15f22b
Miltiadis Stouras <m.stouras@gmail.com>
2018-04-03 14:52:04 +0300

    Changed fetch to request data with cache mode : 'reload'

--- telemetry-data-plotting/src/telemetryPlot.js
+++ telemetry-data-plotting/src/telemetryPlot.js
@@ -10,7 +10,7 @@ class Telemetry extends Component {
     this.state = initialState();
 
     setInterval(() => {
-      fetch('http://localhost:3000/TelemetryData.json')
+      fetch('http://localhost:5000/TelemetryData.json',{cache: "reload"})
       .then(response => {
          try{
            return response.json();
