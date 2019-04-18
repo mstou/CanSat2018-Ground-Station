0c88f2e9c116362281ed3ad039686bf594a1a2e1
Miltiadis Stouras <m.stouras@gmail.com>
2019-04-18 22:44:22 +0300

    Add filtering for rubbish packets

--- buildJSON.py
+++ buildJSON.py
@@ -4,8 +4,21 @@ serialPort = serial.Serial('/dev/ttyUSB0');
 dataToPrint = []
 quote = "\""
 
+
+def checkPacketValidity(packet):
+    for x in packet:
+        if !(x.isdigit() or x=="." or x=="," or x=="\n"):
+            return false
+    return true
+         
+
+
 while (73):
     telemetryPackage = serialPort.readline() #waits indefinetly for the next packet
+
+    if !checkPacketValidity(telemetryPackage[:-2]):
+        continue
+
     telemetryPackage = quote + telemetryPackage[:-2] + quote
     #add "," to current last element.
     if (len(dataToPrint) > 0):
